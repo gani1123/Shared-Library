28e7c3eec24084db6646d6953ca0dab2c45ffe72
gani1123 <ganeshnalli1123@gmail.com>
2026-03-25 23:40:27 +0530

    Fix credentials ID for Docker login

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,5 +1,5 @@
 def call(imageName) {
-    withCredentials([usernamePassword(credentialsId: 'docker-creds',
+    withCredentials([usernamePassword(credentialsId: 'docker',
         usernameVariable: 'USER', passwordVariable: 'PASS')]) {
 
         sh "echo $PASS | docker login -u $USER --password-stdin"
