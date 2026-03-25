85aadec9d3212fa36c0e518f9bcaf668ad72e337
gani1123 <ganeshnalli1123@gmail.com>
2026-03-25 23:43:19 +0530

    Update dockerPush.groovy to build and push image

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -2,7 +2,7 @@ def call(imageName) {
     withCredentials([usernamePassword(credentialsId: 'docker',
         usernameVariable: 'USER', passwordVariable: 'PASS')]) {
 
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh "docker push ${imageName}"
+        sh 'docker build -t ganeshnalli/myapp:latest .'
+        sh 'docker push ganeshnalli/myapp:latest'
     }
 }
